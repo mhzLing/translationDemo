35993fc83c6cc29d0867469e03f7ecbee33445fb
Morgan Ling <morganhz.ling@gmail.com>
2017-11-10 18:43:41 -0800

    deleted useless files

--- scripts/index.js
+++
@@ -1,8 +0,0 @@
-//test
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/tag";
-
-var saveTags = function() {
-  var save = getDocumentById('planetmap');
-  console.log(save.outerHTML);
-};
